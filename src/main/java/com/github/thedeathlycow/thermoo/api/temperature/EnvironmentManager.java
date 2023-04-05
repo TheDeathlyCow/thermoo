2e04b7dcc4088a9a1eb676f5dcdba6bf5cb32a04
TheDeathlyCow <thedeathlycow@gmail.com>
2023-04-04 17:41:37 -1000

    Revert "controller config"
    This reverts commit 03126f497ce633de7474ca183ca813bc96ebd4e3.

--- src/main/java/com/github/thedeathlycow/thermoo/api/temperature/EnvironmentManager.java
+++ src/main/java/com/github/thedeathlycow/thermoo/api/temperature/EnvironmentManager.java
@@ -1,12 +1,6 @@
 package com.github.thedeathlycow.thermoo.api.temperature;
 
 import com.github.thedeathlycow.thermoo.impl.EnvironmentControllerImpl;
-import com.github.thedeathlycow.thermoo.impl.Thermoo;
-import com.github.thedeathlycow.thermoo.impl.config.ThermooConfig;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 
 /**
  * Controls what instance of the {@link EnvironmentManager} is to be used by Thermoo events
@@ -18,59 +12,30 @@ public final class EnvironmentManager {
      */
     public static final EnvironmentManager INSTANCE = new EnvironmentManager();
 
-    private final Map<String, EnvironmentController> controllers = new HashMap<>();
-    private String controller;
+    private EnvironmentController controller;
 
     /**
      * @return Returns the current default {@link EnvironmentController} used by Thermoo events
      */
     public EnvironmentController getController() {
-        return this.controllers.get(controller);
+        return controller;
     }
 
     /**
-     * Gets an {@link EnvironmentController} by name
-     *
-     * @param name The name of the controller
-     * @return The controller
-     */
-    public Optional<EnvironmentController> getControllerByName(String name) {
-        return Optional.ofNullable(this.controllers.get(name));
-    }
-
-    /**
-     * Adds a new {@link EnvironmentController} to be used by Thermoo events with a specified name.
+     * Sets the default {@link EnvironmentController} to be used by Thermoo events.
      * <p>
-     * Which controller ends up being used is determined by the Thermoo config.
+     * If this is NOT set, then it will be set to a default instance provided by Thermoo.
+     * <p>
+     * NOTE: This will override the existing event, so be careful that no other mods are using this!
      *
-     * @param name       The name of the controller. It is recommended to use your modid for this.
      * @param controller The controller to set
      */
-    public void putController(String name, EnvironmentController controller) {
-        this.controllers.put(name, controller);
-    }
-
-    /**
-     * Sets the current controller to be used by Thermoo.
-     * <p>
-     * If the controller has not been added to the manager by {@link EnvironmentManager#putController(String, EnvironmentController)},
-     * then an {@link IllegalArgumentException} will be thrown.
-     *
-     * @param name The name of the controller to be used by Thermoo
-     */
-    public void setController(String name) {
-        if (this.controllers.containsKey(name)) {
-            this.controller = name;
-        } else {
-            throw new IllegalArgumentException(
-                    String.format("Cannot set controller '%s' as it does not exist in the environment manager", name)
-            );
-        }
+    public void setController(EnvironmentController controller) {
+        this.controller = controller;
     }
 
     private EnvironmentManager() {
-        this.putController(Thermoo.MODID, new EnvironmentControllerImpl());
-        this.setController(Thermoo.getConfig().environmentConfig.getController());
+        this.controller = new EnvironmentControllerImpl();
     }
 
 }
