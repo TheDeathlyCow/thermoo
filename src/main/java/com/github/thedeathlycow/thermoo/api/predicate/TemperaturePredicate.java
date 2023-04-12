0fdea1ec656ec2d693d0783e678687100dc09d6e
TheDeathlyCow <thedeathlycow@gmail.com>
2023-04-11 20:24:42 -1000

    temperature predicate javadoc

--- src/main/java/com/github/thedeathlycow/thermoo/api/predicate/TemperaturePredicate.java
+++ src/main/java/com/github/thedeathlycow/thermoo/api/predicate/TemperaturePredicate.java
@@ -9,16 +9,58 @@ import net.minecraft.util.JsonHelper;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Predicate for testing the temperature of a {@link TemperatureAware}
+ * <p>
+ * This is injected into {@link net.minecraft.predicate.entity.EntityPredicate} via mixin.
+ * This mean that this is NOT a new loot condition, it is actually an extension of the vanilla predicate
+ * {@link net.minecraft.predicate.entity.EntityPredicate} and can therefore be used anywhere that it is used. For exameple,
+ * as part of the vanilla loot condition {@code minecraft:entity_properties}
+ *
+ * Example usage:
+ * <pre>
+ * {
+ * 	    "condition": "minecraft:entity_properties",
+ * 	    "entity": "this",
+ * 	    "predicate": {
+ * 		    "thermoo.temperature": {
+ * 			    "scale": {@link NumberRange.FloatRange},
+ * 			    "temperature": {@link NumberRange.IntRange}
+ *          }
+ *      }
+ * }
+ * </pre>
+ *
+ * Note that both {@code scale} and {@code temperature} are optional, and if not supplied will default to
+ * {@link NumberRange.FloatRange#ANY} and {@link NumberRange.IntRange#ANY} respectively.
+ *
+ * @see net.minecraft.predicate.entity.EntityPredicate
+ */
 public class TemperaturePredicate {
 
+    /**
+     * "Any" predicate that will always return as true when tested
+     */
     public static final TemperaturePredicate ANY = new TemperaturePredicate(
             NumberRange.FloatRange.ANY,
             NumberRange.IntRange.ANY
     );
 
+    /**
+     * The temperature scale range accepted by this predicate
+     */
     private final NumberRange.FloatRange scaleRange;
+    /**
+     * The absolute temperature value range accepted by this predicate
+     */
     private final NumberRange.IntRange temperatureRange;
 
+    /**
+     * Constructs a new temperature predicate with a scale and temperature range
+     *
+     * @param scaleRange       The temperature scale range accepted by this predicate
+     * @param temperatureRange The absolute temperature value range accepted by this predicate
+     */
     public TemperaturePredicate(
             NumberRange.FloatRange scaleRange,
             NumberRange.IntRange temperatureRange
@@ -27,6 +69,17 @@ public class TemperaturePredicate {
         this.temperatureRange = temperatureRange;
     }
 
+    /**
+     * Tests the temperature of a {@link TemperatureAware} against this predicate.
+     * <p>
+     * If this predicate is {@link #ANY} then this test will always be {@code true}.
+     * <p>
+     * Otherwise, BOTH the temperature scale and temperature value of the {@code tempAware} must match
+     * the ranges of this predicate
+     *
+     * @param tempAware The {@link TemperatureAware} to test
+     * @return Returns true if the predicate accepts {@code tempAware}
+     */
     public boolean test(TemperatureAware tempAware) {
         if (this == ANY) {
             return true;
@@ -38,6 +91,13 @@ public class TemperaturePredicate {
         return this.scaleRange.test(scale) && this.temperatureRange.test(temp);
     }
 
+    /**
+     * Serializes this predicate to JSON
+     * <p>
+     * If this instance is {@link #ANY}, then returns {@link JsonNull}
+     *
+     * @return Returns a {@link JsonElement} that represents this instance
+     */
     public JsonElement toJson() {
         if (this == ANY) {
             return JsonNull.INSTANCE;
@@ -49,6 +109,14 @@ public class TemperaturePredicate {
         return jsonObject;
     }
 
+    /**
+     * Returns the {@link TemperaturePredicate} that is represented by the given {@code json}.
+     * <p>
+     * If the given {@code json} is null or {@link JsonNull}, then returns {@link #ANY}
+     *
+     * @param json The json object to deserialize
+     * @return Returns the temperature predicate represented by the given {@code json}.
+     */
     @Contract("!null->new")
     public static TemperaturePredicate fromJson(@Nullable JsonElement json) {
         if (json == null || json.isJsonNull()) {
@@ -71,6 +139,11 @@ public class TemperaturePredicate {
         return new TemperaturePredicate(scaleRange, tempRange);
     }
 
+    /**
+     * Builder class for {@link TemperaturePredicate}
+     *
+     * @see TemperaturePredicate
+     */
     public static class Builder {
         private NumberRange.FloatRange scale = NumberRange.FloatRange.ANY;
         private NumberRange.IntRange temperature = NumberRange.IntRange.ANY;
