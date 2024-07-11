a7489fa4eb91f7af111121ed809080c1b18d7907
TheDeathlyCow <thedeathlycow@gmail.com>
2024-07-10 21:20:29 -1000

    add javadoc

--- src/main/java/com/github/thedeathlycow/thermoo/api/armor/material/ArmorMaterialEvents.java
+++ src/main/java/com/github/thedeathlycow/thermoo/api/armor/material/ArmorMaterialEvents.java
@@ -6,8 +6,22 @@ import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ArmorMaterial;
 import net.minecraft.registry.entry.RegistryEntry;
 
+/**
+ * Events for getting the thermal resistances of various {@linkplain ArmorMaterial armor materials}
+ */
 public class ArmorMaterialEvents {
 
+    /**
+     * Gets the {@linkplain com.github.thedeathlycow.thermoo.api.ThermooAttributes#HEAT_RESISTANCE heat resistance}
+     * for a particular {@linkplain ArmorMaterial armor material} and {@linkplain ArmorItem.Type armor type}.
+     * <p>
+     * If returns 0 or {@link Double#NaN} then no heat resistance is applied.
+     * <p>
+     * It is recommended to base the resistance amount on the material {@linkplain ArmorMaterialTags tag}.
+     * <p>
+     * Known issue: using tags is not world-specific, so please do not use these tags in datapacks that aren't meant to
+     * be applied globally.
+     */
     public static final Event<GetResistance> GET_HEAT_RESISTANCE = EventFactory.createArrayBacked(
             GetResistance.class,
             listeners -> (armorMaterial, armorType) -> {
@@ -22,6 +36,17 @@ public class ArmorMaterialEvents {
             }
     );
 
+    /**
+     * Gets the {@linkplain com.github.thedeathlycow.thermoo.api.ThermooAttributes#FROST_RESISTANCE frost resistance}
+     * for a particular {@linkplain ArmorMaterial armor material} and {@linkplain ArmorItem.Type armor type}.
+     * <p>
+     * If returns 0 or {@link Double#NaN} then no frost resistance is applied.
+     * <p>
+     * It is recommended to base the resistance amount on the material {@linkplain ArmorMaterialTags tag}.
+     * <p>
+     * Known issue: using tags is not world-specific, so please do not use these tags in datapacks that aren't meant to
+     * be applied globally.
+     */
     public static final Event<GetResistance> GET_FROST_RESISTANCE = EventFactory.createArrayBacked(
             GetResistance.class,
             listeners -> (armorMaterial, armorType) -> {
