bbc59daca279cb97ff1b1f8007aa3cc6e7fd8ed0
TheDeathlyCow <53499406+TheDeathlyCow@users.noreply.github.com>
2025-01-14 21:18:50 -1000

    Attribute Events (#43)
    * extract some logic from mixin method
    
    * convert to event system
    
    * add gametest runner
    
    * reduce number of builds needed for check
    
    * add javadoc
    
    * add deprecated tag to env controller subclasses

---
+++ src/main/java/com/github/thedeathlycow/thermoo/impl/attribute/AttributeHelper.java
@@ -0,0 +1,50 @@
+package com.github.thedeathlycow.thermoo.impl.attribute;
+
+import net.minecraft.datafixer.schema.IdentifierNormalizingSchema;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+
+/**
+ * Helper methods for attribute related functions
+ */
+public class AttributeHelper {
+
+    private static final String PREFIX = "thermoo:generic.";
+
+    public static String fixPrefixedAttributeIds(String id) {
+        String normalizedID = IdentifierNormalizingSchema.normalize(id);
+
+        String normalizedPrefix = IdentifierNormalizingSchema.normalize(PREFIX);
+        if (normalizedID.startsWith(normalizedPrefix)) {
+            return "thermoo:" + normalizedID.substring(normalizedPrefix.length());
+        }
+
+        return id;
+    }
+
+    public static void applyValueAsModifier(
+            LivingEntity entity,
+            AttributeData attribute,
+            double value
+    ) {
+        var modifier = new EntityAttributeModifier(
+                attribute.id(),
+                value,
+                EntityAttributeModifier.Operation.ADD_VALUE
+        );
+
+        EntityAttributeInstance attributeInstance = entity.getAttributeInstance(attribute.attribute());
+
+        if (attributeInstance == null) {
+            throw new IllegalStateException("Attribute not found on " + entity.getType() + ": " + attribute);
+        }
+
+        attributeInstance.addTemporaryModifier(modifier);
+    }
+
+    private AttributeHelper() {
+
+    }
+
+}
--- src/testmod/java/com/github/thedeathlycow/thermoo/testmod/ThermooTestMod.java
+++ src/testmod/java/com/github/thedeathlycow/thermoo/testmod/ThermooTestMod.java
@@ -1,6 +1,7 @@
 package com.github.thedeathlycow.thermoo.testmod;
 
 import com.github.thedeathlycow.thermoo.api.armor.material.ArmorMaterialEvents;
+import com.github.thedeathlycow.thermoo.api.ThermooAttributes;
 import com.github.thedeathlycow.thermoo.api.temperature.event.EnvironmentControllerInitializeEvent;
 import com.github.thedeathlycow.thermoo.api.temperature.event.PlayerEnvironmentEvents;
 import com.github.thedeathlycow.thermoo.impl.Thermoo;
@@ -39,6 +40,8 @@ public class ThermooTestMod implements ModInitializer {
 
         ArmorMaterialEvents.GET_FROST_RESISTANCE.register(ArmorMaterialListener.COLD);
         ArmorMaterialEvents.GET_HEAT_RESISTANCE.register(ArmorMaterialListener.HEAT);
+        ThermooAttributes.baseValueEvent(ThermooAttributes.MIN_TEMPERATURE).register((entity, baseValue) -> 40);
+        ThermooAttributes.baseValueEvent(ThermooAttributes.MAX_TEMPERATURE).register((entity, baseValue) -> 40);
     }
 
     public static ThermooConfig getConfig() {
