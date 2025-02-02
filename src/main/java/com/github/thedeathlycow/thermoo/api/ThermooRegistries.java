3457acb42b120c4e6bb5bc5b8f7f974d41b5ee4a
TheDeathlyCow <thedeathlycow@gmail.com>
2025-02-02 05:18:20 -1000

    add env provider registry

--- src/main/java/com/github/thedeathlycow/thermoo/api/ThermooRegistries.java
+++ src/main/java/com/github/thedeathlycow/thermoo/api/ThermooRegistries.java
@@ -1,5 +1,6 @@
 package com.github.thedeathlycow.thermoo.api;
 
+import com.github.thedeathlycow.thermoo.api.environment.EnvironmentProvider;
 import com.github.thedeathlycow.thermoo.api.temperature.effects.TemperatureEffect;
 import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder;
 import net.minecraft.registry.Registry;
@@ -17,6 +18,11 @@ public final class ThermooRegistries {
     public static final Registry<TemperatureEffect<?>> TEMPERATURE_EFFECTS =
             FabricRegistryBuilder.createSimple(
                     ThermooRegistryKeys.TEMPERATURE_EFFECT
+            ).buildAndRegister();
+
+    public static final Registry<EnvironmentProvider> ENVIRONMENT_PROVIDER =
+            FabricRegistryBuilder.createSimple(
+                    ThermooRegistryKeys.ENVIRONMENT_PROVIDER
             ).buildAndRegister();
 
     private ThermooRegistries() {
--- src/main/java/com/github/thedeathlycow/thermoo/api/ThermooRegistryKeys.java
+++ src/main/java/com/github/thedeathlycow/thermoo/api/ThermooRegistryKeys.java
@@ -1,5 +1,6 @@
 package com.github.thedeathlycow.thermoo.api;
 
+import com.github.thedeathlycow.thermoo.api.environment.EnvironmentProvider;
 import com.github.thedeathlycow.thermoo.api.temperature.effects.TemperatureEffect;
 import com.github.thedeathlycow.thermoo.impl.Thermoo;
 import net.minecraft.registry.Registry;
@@ -9,6 +10,7 @@ public final class ThermooRegistryKeys {
 
 
     public static final RegistryKey<Registry<TemperatureEffect<?>>> TEMPERATURE_EFFECT = createRegistryKey("temperature_effects");
+    public static final RegistryKey<Registry<EnvironmentProvider>> ENVIRONMENT_PROVIDER = createRegistryKey("environment_provider");
 
     private static <T> RegistryKey<Registry<T>> createRegistryKey(String registryId) {
         return RegistryKey.ofRegistry(Thermoo.id(registryId));
