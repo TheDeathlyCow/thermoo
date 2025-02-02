a8d55d0054354560b72b135de4a9273ea11c8d4e
TheDeathlyCow <thedeathlycow@gmail.com>
2025-02-02 05:17:01 -1000

    temperate and tropical providers

--- src/main/java/com/github/thedeathlycow/thermoo/api/environment/SeasonalEnvironmentProvider.java
+++ src/main/java/com/github/thedeathlycow/thermoo/api/environment/SeasonalEnvironmentProvider.java
@@ -8,19 +8,26 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 
+import java.util.Map;
 import java.util.Optional;
 
 public abstract class SeasonalEnvironmentProvider extends EnvironmentProvider {
 
     private final EnvironmentProvider fallback;
+    private final Map<ThermooSeason, EnvironmentProvider> seasons;
 
-    protected SeasonalEnvironmentProvider(RegistryEntryList<Biome> biomes, EnvironmentProvider fallback) {
+    protected SeasonalEnvironmentProvider(
+            RegistryEntryList<Biome> biomes,
+            EnvironmentProvider fallback,
+            Map<ThermooSeason, EnvironmentProvider> seasons
+    ) {
         super(biomes);
         this.fallback = fallback;
+        this.seasons = seasons;
     }
 
     @Override
-    public double getTemperature(World world, BlockPos pos, RegistryEntry<Biome> biome, TemperatureUnit unit) {
+    public final double getTemperature(World world, BlockPos pos, RegistryEntry<Biome> biome, TemperatureUnit unit) {
         Optional<ThermooSeason> season = ThermooSeason.getCurrentSeason(world);
         if (season.isEmpty()) {
             return this.fallback.getTemperature(world, pos, biome, unit);
@@ -32,7 +39,7 @@ public abstract class SeasonalEnvironmentProvider extends EnvironmentProvider {
     }
 
     @Override
-    public double getHumidity(World world, BlockPos pos, RegistryEntry<Biome> biome) {
+    public final double getHumidity(World world, BlockPos pos, RegistryEntry<Biome> biome) {
         Optional<ThermooSeason> season = ThermooSeason.getCurrentSeason(world);
         if (season.isEmpty()) {
             return this.fallback.getHumidity(world, pos, biome);
@@ -43,5 +50,7 @@ public abstract class SeasonalEnvironmentProvider extends EnvironmentProvider {
                 .orElse(this.fallback.getHumidity(world, pos, biome));
     }
 
-    protected abstract Optional<EnvironmentProvider> getForSeason(ThermooSeason season);
+    protected Optional<EnvironmentProvider> getForSeason(ThermooSeason season) {
+        return Optional.ofNullable(this.seasons.get(season));
+    }
 }
---
+++ src/main/java/com/github/thedeathlycow/thermoo/api/environment/TropicalSeasonEnvironmentProvider.java
@@ -0,0 +1,22 @@
+package com.github.thedeathlycow.thermoo.api.environment;
+
+import com.github.thedeathlycow.thermoo.api.season.ThermooSeason;
+import net.minecraft.registry.entry.RegistryEntryList;
+import net.minecraft.world.biome.Biome;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class TropicalSeasonEnvironmentProvider extends SeasonalEnvironmentProvider {
+    public TropicalSeasonEnvironmentProvider(RegistryEntryList<Biome> biomes, EnvironmentProvider fallback, Map<ThermooSeason, EnvironmentProvider> seasons) {
+        super(biomes, fallback, seasons);
+    }
+
+    @Override
+    protected Optional<EnvironmentProvider> getForSeason(ThermooSeason season) {
+        if (season.isTropical()) {
+            return super.getForSeason(season);
+        }
+        return Optional.empty();
+    }
+}
