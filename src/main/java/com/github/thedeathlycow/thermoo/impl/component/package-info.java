6714a751029ea2f1759bbef96de495936824cb41
TheDeathlyCow <thedeathlycow@gmail.com>
2025-02-03 01:51:17 -1000

    add internal annotation to all impl sub packages

---
+++ src/main/java/com/github/thedeathlycow/thermoo/impl/component/package-info.java
@@ -0,0 +1,4 @@
+@ApiStatus.Internal
+package com.github.thedeathlycow.thermoo.impl.component;
+
+import org.jetbrains.annotations.ApiStatus;
---
+++ src/main/java/com/github/thedeathlycow/thermoo/impl/temperature/effect/package-info.java
@@ -0,0 +1,4 @@
+@ApiStatus.Internal
+package com.github.thedeathlycow.thermoo.impl.temperature.effect;
+
+import org.jetbrains.annotations.ApiStatus;
