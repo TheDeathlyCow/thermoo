e251040a00878defeec57c0fa2e2051ffcc9c646
TheDeathlyCow <thedeathlycow@gmail.com>
2025-02-04 17:48:34 -1000

    add absolute unit to temperature unit

--- src/main/java/com/github/thedeathlycow/thermoo/api/util/TemperatureUnit.java
+++ src/main/java/com/github/thedeathlycow/thermoo/api/util/TemperatureUnit.java
@@ -4,6 +4,7 @@ import com.mojang.serialization.Codec;
 import net.minecraft.util.StringIdentifiable;
 
 import java.util.function.DoubleUnaryOperator;
+import java.util.function.Supplier;
 
 /**
  * Defines the basic units of temperature and allows for conversions between them.
@@ -13,22 +14,26 @@ public enum TemperatureUnit implements StringIdentifiable {
     CELSIUS(
             "C",
             celsiusValue -> celsiusValue,
-            celsiusValue -> celsiusValue
+            celsiusValue -> celsiusValue,
+            1
     ),
     KELVIN(
             "K",
             kelvinValue -> kelvinValue - 273.15,
-            celsiusValue -> celsiusValue + 273.15
+            celsiusValue -> celsiusValue + 273.15,
+            1
     ),
     FAHRENHEIT(
             "F",
             fahrenheitValue -> (fahrenheitValue - 32.0) * 5.0 / 9.0,
-            celsiusValue -> (9.0 / 5.0) * celsiusValue + 32.0
+            celsiusValue -> (9.0 / 5.0) * celsiusValue + 32.0,
+            3
     ),
     RANKINE(
             "R",
             rankineValue -> FAHRENHEIT.toCelsius(rankineValue - 459.67),
-            celsiusValue -> FAHRENHEIT.fromCelsius(celsiusValue) + 459.67
+            celsiusValue -> FAHRENHEIT.fromCelsius(celsiusValue) + 459.67,
+            3
     );
 
     public static final Codec<TemperatureUnit> CODEC = StringIdentifiable.createCodec(TemperatureUnit::values);
@@ -39,10 +44,32 @@ public enum TemperatureUnit implements StringIdentifiable {
 
     private final DoubleUnaryOperator fromCelsius;
 
-    TemperatureUnit(String unitSymbol, DoubleUnaryOperator toCelsius, DoubleUnaryOperator fromCelsius) {
+    private final int absoluteUnitIndex;
+
+    TemperatureUnit(String unitSymbol, DoubleUnaryOperator toCelsius, DoubleUnaryOperator fromCelsius, int absoluteUnitIndex) {
         this.unitSymbol = unitSymbol;
         this.toCelsius = toCelsius;
         this.fromCelsius = fromCelsius;
+        this.absoluteUnitIndex = absoluteUnitIndex;
+    }
+
+    /**
+     * Gets the absolute unit scale for this unit. That is, the unit that has the same temperature step as this unit,
+     * but whose origin point (0 degrees) is <a href="https://en.wikipedia.org/wiki/Absolute_zero">absolute zero</a>.
+     *
+     * @return If this unit is Celsius or Kelvin, returns Kelvin. If this unit is Fahrenheit or Rankine, returns Rankine.
+     */
+    public TemperatureUnit getAbsoluteUnit() {
+        return values()[this.absoluteUnitIndex];
+    }
+
+    /**
+     * Gets the absolute zero temperature value of this unit
+     *
+     * @return Returns 0 if this is the absolute unit, or some other negative value if it is not the absolute unit.
+     */
+    public double getAbsoluteZero() {
+        return this.convertTemperature(0, this.getAbsoluteUnit());
     }
 
     public String getUnitSymbol() {
