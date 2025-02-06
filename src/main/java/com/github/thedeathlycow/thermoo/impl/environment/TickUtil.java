a07f7984b722b943451687cc5c8431f2075cea1d
TheDeathlyCow <53499406+TheDeathlyCow@users.noreply.github.com>
2025-02-05 16:56:29 -1000

    Server player environment tick events (#50)
    * player environment tick event
    
    * allow temperature change
    
    * deprecate old can apply temp change event
    
    * allow update event and javadoc

---
+++ src/main/java/com/github/thedeathlycow/thermoo/impl/environment/TickUtil.java
@@ -0,0 +1,96 @@
+package com.github.thedeathlycow.thermoo.impl.environment;
+
+import com.github.thedeathlycow.thermoo.api.environment.EnvironmentLookup;
+import com.github.thedeathlycow.thermoo.api.environment.event.EnvironmentTickContext;
+import com.github.thedeathlycow.thermoo.api.environment.event.ServerPlayerEnvironmentTickEvents;
+import com.github.thedeathlycow.thermoo.api.temperature.HeatingModes;
+import com.github.thedeathlycow.thermoo.api.temperature.TemperatureAware;
+import com.github.thedeathlycow.thermoo.api.util.TemperatureRecord;
+import com.github.thedeathlycow.thermoo.api.util.TemperatureUnit;
+import net.fabricmc.fabric.api.util.TriState;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import org.jetbrains.annotations.NotNull;
+
+public final class TickUtil {
+    public static void tickPlayerTemperature(ServerPlayerEntity player) {
+        final EnvironmentTickContextImpl<ServerPlayerEntity> context = new EnvironmentTickContextImpl<>(
+                player,
+                player.getServerWorld(),
+                player.getBlockPos()
+        );
+        if (ServerPlayerEnvironmentTickEvents.ALLOW_TEMPERATURE_UPDATE.invoker().allowUpdate(context) == TriState.FALSE) {
+            return;
+        }
+
+
+        final var lookup = EnvironmentLookup.getInstance();
+        final var unit = TemperatureUnit.CELSIUS;
+
+        final var temperature = new TemperatureRecord(
+                lookup.findTemperature(context.world, context.pos, unit),
+                unit
+        );
+        final double relativeHumidity = lookup.findRelativeHumidity(context.world, context.pos);
+
+        context.temperature = temperature;
+        context.relativeHumidity = relativeHumidity;
+
+
+        int temperatureChange = ServerPlayerEnvironmentTickEvents.GET_TEMPERATURE_CHANGE.invoker().addPointChange(context);
+
+        if (temperatureChange != 0 && invokeAllowChange(context, temperatureChange)) {
+            player.thermoo$addTemperature(temperatureChange, HeatingModes.PASSIVE);
+        }
+    }
+
+    private static boolean invokeAllowChange(EnvironmentTickContext<ServerPlayerEntity> context, int temperatureChange) {
+        TriState result = ServerPlayerEnvironmentTickEvents.ALLOW_TEMPERATURE_CHANGE.invoker()
+                .allowTemperatureChange(context, temperatureChange);
+        return result != TriState.FALSE;
+    }
+
+    private static class EnvironmentTickContextImpl<T extends TemperatureAware> implements EnvironmentTickContext<T> {
+
+        private final T affected;
+        private final ServerWorld world;
+        private final BlockPos pos;
+        private TemperatureRecord temperature = null;
+        private double relativeHumidity = Double.NaN;
+
+        public EnvironmentTickContextImpl(T affected, ServerWorld world, BlockPos pos) {
+            this.affected = affected;
+            this.world = world;
+            this.pos = pos;
+        }
+
+        @Override
+        public @NotNull T affected() {
+            return this.affected;
+        }
+
+        @Override
+        public @NotNull ServerWorld world() {
+            return this.world;
+        }
+
+        @Override
+        public @NotNull BlockPos pos() {
+            return this.pos;
+        }
+
+        @Override
+        public TemperatureRecord temperature() {
+            return this.temperature;
+        }
+
+        @Override
+        public double relativeHumidity() {
+            return this.relativeHumidity;
+        }
+    }
+
+    private TickUtil() {
+    }
+}
