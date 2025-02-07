1b09df937ef90f918442c2bd0ef8b66787cd32f0
TheDeathlyCow <thedeathlycow@gmail.com>
2025-02-07 03:24:49 -1000

    dark forest winter modifier

--- src/testmod/java/com/github/thedeathlycow/thermoo/testmod/tests/environment/DarkForestTests.java
+++ src/testmod/java/com/github/thedeathlycow/thermoo/testmod/tests/environment/DarkForestTests.java
@@ -1,5 +1,6 @@
 package com.github.thedeathlycow.thermoo.testmod.tests.environment;
 
+import com.github.thedeathlycow.thermoo.api.season.ThermooSeason;
 import net.fabricmc.fabric.api.gametest.v1.FabricGameTest;
 import net.minecraft.test.GameTest;
 import net.minecraft.test.TestContext;
@@ -26,6 +27,28 @@ public class DarkForestTests {
 
         double humidity = EnvironmentTestHelper.getBiomeHumidity(context, world, BiomeKeys.DARK_FOREST);
         EnvironmentTestHelper.assertHumidityEquals(context, 0.51, humidity);
+
+        context.complete();
+    }
+
+    @GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE)
+    public void dark_forest_winter_temperature_is_0c(TestContext context) {
+        World world = context.getWorld();
+        EnvironmentTestHelper.setSeasons(context, ThermooSeason.WINTER, null);
+
+        double temperature = EnvironmentTestHelper.getBiomeTemperature(context, world, BiomeKeys.DARK_FOREST);
+        EnvironmentTestHelper.assertTemperatureEquals(context, 0.0, temperature);
+
+        context.complete();
+    }
+
+    @GameTest(templateName = FabricGameTest.EMPTY_STRUCTURE)
+    public void dark_forest_winter_humidity_is_25pc(TestContext context) {
+        World world = context.getWorld();
+        EnvironmentTestHelper.setSeasons(context, ThermooSeason.WINTER, null);
+
+        double humidity = EnvironmentTestHelper.getBiomeHumidity(context, world, BiomeKeys.DARK_FOREST);
+        EnvironmentTestHelper.assertHumidityEquals(context, 0.25, humidity);
 
         context.complete();
     }
