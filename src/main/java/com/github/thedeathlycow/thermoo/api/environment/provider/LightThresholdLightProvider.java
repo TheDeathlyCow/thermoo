9dd52c0d6c9c4d2c6748382d977ae9ac1337bc23
TheDeathlyCow <thedeathlycow@gmail.com>
2025-02-11 00:09:27 -1000

    light threshold provider javadoc

--- src/main/java/com/github/thedeathlycow/thermoo/api/environment/provider/LightThresholdLightProvider.java
+++ src/main/java/com/github/thedeathlycow/thermoo/api/environment/provider/LightThresholdLightProvider.java
@@ -5,7 +5,6 @@ import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.component.ComponentMap;
 import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.util.StringIdentifiable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.LightType;
 import net.minecraft.world.World;
@@ -15,12 +14,19 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Optional;
 
+/**
+ * Used to pick between two child providers based on a light level threshold. Can filter for {@link LightType} and apply
+ * or ignore {@link World#getAmbientDarkness() ambient darkness} to sky light.
+ */
 public class LightThresholdLightProvider implements EnvironmentProvider {
     public static final MapCodec<LightThresholdLightProvider> CODEC = RecordCodecBuilder.mapCodec(
             instance -> instance.group(
-                    LightTypeWrapper.CODEC
+                    Codec.stringResolver(
+                                    light -> light.name().toLowerCase(),
+                                    name -> LightType.valueOf(name.toUpperCase())
+                            )
                             .optionalFieldOf("light_type")
-                            .forGetter(LightThresholdLightProvider::lightTypeWrapper),
+                            .forGetter(LightThresholdLightProvider::lightType),
                     Codec.BOOL
                             .optionalFieldOf("apply_ambient_darkness", true)
                             .forGetter(LightThresholdLightProvider::applyAmbientDarkness),
@@ -36,12 +42,20 @@ public class LightThresholdLightProvider implements EnvironmentProvider {
             ).apply(instance, LightThresholdLightProvider::new)
     );
 
-    private final Optional<LightTypeWrapper> lightTypeWrapper;
+    private final Optional<LightType> lightType;
     private final boolean applyAmbientDarkness;
     private final int threshold;
     private final RegistryEntry<EnvironmentProvider> above;
     private final RegistryEntry<EnvironmentProvider> below;
 
+    /**
+     * Creates a new builder with the mandatory threshold, above, and below fields
+     *
+     * @param threshold The light level threshold
+     * @param above     The provider to use when a positions light level is at or above the {@code threshold}
+     * @param below     The provider to use when a positions light level is below the {@code threshold}
+     * @return Returns a new builder instance
+     */
     @Contract("_,_,_->new")
     public static Builder builder(
             int threshold,
@@ -52,26 +66,37 @@ public class LightThresholdLightProvider implements EnvironmentProvider {
     }
 
     private LightThresholdLightProvider(
-            Optional<LightTypeWrapper> lightTypeWrapper,
+            Optional<LightType> lightType,
             boolean applyAmbientDarkness,
             int threshold,
             RegistryEntry<EnvironmentProvider> above,
             RegistryEntry<EnvironmentProvider> below
     ) {
-        this.lightTypeWrapper = lightTypeWrapper;
+        this.lightType = lightType;
         this.applyAmbientDarkness = applyAmbientDarkness;
         this.threshold = threshold;
         this.above = above;
         this.below = below;
     }
 
+    /**
+     * Finds the current components of the world position based on light level. If the light level of the position is at
+     * or above the threshold then returns the {@link #above()} provider. Otherwise, returns the {@link #below()} provider.
+     * <p>
+     * Filters for sky/block light and ambient darkness if requested.
+     *
+     * @param world The world/level being queried
+     * @param pos   The position in the world to query
+     * @param biome The biome at the position in the world
+     * @return Returns one of the child providers based on light conditions
+     */
     @Override
     public ComponentMap findCurrentComponents(World world, BlockPos pos, RegistryEntry<Biome> biome) {
-        int lightLevel = this.lightTypeWrapper
-                .map(type -> world.getLightLevel(type.lightType(), pos))
+        int lightLevel = this.lightType
+                .map(type -> world.getLightLevel(type, pos))
                 .orElseGet(() -> world.getLightLevel(pos));
 
-        if (this.applyAmbientDarkness && this.lightTypeWrapper.orElse(null) == LightTypeWrapper.SKY) {
+        if (this.applyAmbientDarkness && this.lightType.orElse(null) == LightType.SKY) {
             lightLevel -= world.getAmbientDarkness();
         }
 
@@ -85,30 +110,48 @@ public class LightThresholdLightProvider implements EnvironmentProvider {
         return EnvironmentProviderTypes.LIGHT_THRESHOLD;
     }
 
+    /**
+     * The optional light type of this provider. If not specified, uses {@link net.minecraft.world.WorldView#getLightLevel(BlockPos)}
+     * to determine light level.
+     *
+     * @return Returns the light type of this provider.
+     */
     public Optional<LightType> lightType() {
-        return this.lightTypeWrapper.map(LightTypeWrapper::lightType);
+        return this.lightType;
     }
 
-    private Optional<LightTypeWrapper> lightTypeWrapper() {
-        return this.lightTypeWrapper;
-    }
-
+    /**
+     * Whether ambient darkness should be applied when using the skylight light type (default: true)
+     */
     public boolean applyAmbientDarkness() {
         return this.applyAmbientDarkness;
     }
 
+    /**
+     * Light level threshold that determines whether to use {@link #above()} or {@link #below()} when finding the
+     * environment components
+     */
     public int threshold() {
         return this.threshold;
     }
 
+    /**
+     * The provider to use when the light level is at or above the {@link #threshold()}
+     */
     public RegistryEntry<EnvironmentProvider> above() {
         return this.above;
     }
 
+    /**
+     * The provider to use when the light level is below the {@link #threshold()}
+     */
     public RegistryEntry<EnvironmentProvider> below() {
         return this.below;
     }
 
+    /**
+     * Builder class for light threshold providers
+     */
     public static class Builder {
         @Nullable
         private LightType lightType = null;
@@ -123,59 +166,40 @@ public class LightThresholdLightProvider implements EnvironmentProvider {
             this.below = below;
         }
 
+        /**
+         * Ignore the ambient darkness of the position when using sky light
+         *
+         * @return Returns this builder
+         */
         @Contract("->this")
         public Builder ignoreAmbientDarkness() {
             this.applyAmbientDarkness = false;
             return this;
         }
 
+        /**
+         * Sets a light type to filter on.
+         *
+         * @return Returns this builder
+         */
         @Contract("_->this")
         public Builder withLightType(LightType lightType) {
             this.lightType = lightType;
             return this;
         }
 
+        /**
+         * @return Returns a new provider with the parameters of this builder
+         */
         @Contract("->new")
         public LightThresholdLightProvider build() {
             return new LightThresholdLightProvider(
-                    LightTypeWrapper.of(this.lightType),
+                    Optional.ofNullable(this.lightType),
                     this.applyAmbientDarkness,
                     this.threshold,
                     this.above,
                     this.below
             );
-        }
-    }
-
-    private enum LightTypeWrapper implements StringIdentifiable {
-        BLOCK("block", LightType.BLOCK),
-        SKY("sky", LightType.SKY);
-
-        private static final Codec<LightTypeWrapper> CODEC = StringIdentifiable.createCodec(LightTypeWrapper::values);
-
-        private final String name;
-        private final LightType lightType;
-
-        LightTypeWrapper(String name, LightType lightType) {
-            this.name = name;
-            this.lightType = lightType;
-        }
-
-        private static Optional<LightTypeWrapper> of(@Nullable LightType base) {
-            return switch (base) {
-                case BLOCK -> Optional.of(LightTypeWrapper.BLOCK);
-                case SKY -> Optional.of(LightTypeWrapper.SKY);
-                case null -> Optional.empty();
-            };
-        }
-
-        @Override
-        public String asString() {
-            return this.name;
-        }
-
-        public LightType lightType() {
-            return this.lightType;
         }
     }
 }
