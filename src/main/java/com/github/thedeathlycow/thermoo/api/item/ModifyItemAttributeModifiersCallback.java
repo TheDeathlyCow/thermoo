86d74105ff97992ef8f5779229abcd34b174a93f
TheDeathlyCow <thedeathlycow@gmail.com>
2025-03-04 00:14:50 -1000

    add tests

--- src/main/java/com/github/thedeathlycow/thermoo/api/item/ModifyItemAttributeModifiersCallback.java
+++ src/main/java/com/github/thedeathlycow/thermoo/api/item/ModifyItemAttributeModifiersCallback.java
@@ -6,6 +6,11 @@ import net.minecraft.component.type.AttributeModifiersComponent;
 import net.minecraft.item.ItemStack;
 import org.jetbrains.annotations.ApiStatus;
 
+/**
+ * Stack-aware event for modifying the default attribute modifier component of an item.
+ * <p>
+ * Experimental event, it may not work fully as expected or impact performance. Proceed with caution.
+ */
 @ApiStatus.Experimental
 @FunctionalInterface
 public interface ModifyItemAttributeModifiersCallback {
