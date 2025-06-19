9d998bdc86ea4066206de2e8f78bcfaa61cd3663
TheDeathlyCow <thedeathlycow@gmail.com>
2025-06-19 00:33:53 -1000

    two bars tests

--- src/gametest/java/com/github/thedeathlycow/thermoo/gametest/client/HealthBarTests.java
+++ src/gametest/java/com/github/thedeathlycow/thermoo/gametest/client/HealthBarTests.java
@@ -1,16 +1,20 @@
 package com.github.thedeathlycow.thermoo.gametest.client;
 
+import com.github.thedeathlycow.thermoo.impl.Thermoo;
 import net.fabricmc.fabric.api.client.gametest.v1.FabricClientGameTest;
 import net.fabricmc.fabric.api.client.gametest.v1.context.ClientGameTestContext;
 import net.fabricmc.fabric.api.client.gametest.v1.context.TestSingleplayerContext;
 import net.fabricmc.fabric.api.client.gametest.v1.screenshot.TestScreenshotComparisonOptions;
 
 public class HealthBarTests implements FabricClientGameTest {
-    private static final String EMPTY = "empty_temperature_bar";
-    private static final String HALF_HEART = "half_heart";
-    private static final String FULL_HEART = "full_heart";
-    private static final String HALF_FULL = "half_full_temperature_bar";
-    private static final String FULL = "full_temperature_bar";
+    private static final String EMPTY = "empty";
+    private static final String TWENTIETH = "twentieth";
+    private static final String TENTH = "tenth";
+    private static final String HALF = "half";
+    private static final String FULL = "full";
+    private static final String TWO_BARS = "_two_bars";
+
+    private int offset = 0;
 
     @Override
     public void runTest(ClientGameTestContext context) {
@@ -21,35 +25,49 @@ public class HealthBarTests implements FabricClientGameTest {
         });
 
         try (TestSingleplayerContext singleplayer = context.worldBuilder().create()) {
+            singleplayer.getServer().runCommand("/gamerule fireDamage false"); // hack to prevent damage from affecting health underneath temperature display
             singleplayer.getClientWorld().waitForChunksRender();
 
             final int maxTemperature = 5600;
 
             testPlayerTemperatureBar(singleplayer, context, 0, EMPTY);
             testPlayerTemperatureBar(singleplayer, context, 1, EMPTY);
-            testPlayerTemperatureBar(singleplayer, context, maxTemperature / 20, HALF_HEART);
-            testPlayerTemperatureBar(singleplayer, context, maxTemperature / 10, FULL_HEART);
-            testPlayerTemperatureBar(singleplayer, context, maxTemperature / 10 + 1, FULL_HEART);
-            testPlayerTemperatureBar(singleplayer, context, maxTemperature / 2 - 1, HALF_FULL);
+            testPlayerTemperatureBar(singleplayer, context, maxTemperature / 20, TWENTIETH);
+            testPlayerTemperatureBar(singleplayer, context, maxTemperature / 10, TENTH);
+            testPlayerTemperatureBar(singleplayer, context, maxTemperature / 10 + 1, TENTH);
+            testPlayerTemperatureBar(singleplayer, context, maxTemperature / 2, HALF);
             testPlayerTemperatureBar(singleplayer, context, maxTemperature, FULL);
+
+            singleplayer.getServer().runCommand("/attribute @p max_health base set 40");
+            singleplayer.getServer().runCommand("/effect give @p minecraft:instant_health 4 100 true");
+            context.waitTicks(20); // wait to allow for health bar update
+            offset = 20;
+
+            testPlayerTemperatureBar(singleplayer, context, 0, EMPTY + TWO_BARS);
+            testPlayerTemperatureBar(singleplayer, context, 1, EMPTY + TWO_BARS);
+            testPlayerTemperatureBar(singleplayer, context, maxTemperature / 20, TWENTIETH + TWO_BARS);
+            testPlayerTemperatureBar(singleplayer, context, maxTemperature / 10, TENTH + TWO_BARS);
+            testPlayerTemperatureBar(singleplayer, context, maxTemperature / 10 + 1, TENTH + TWO_BARS);
+            testPlayerTemperatureBar(singleplayer, context, maxTemperature / 2, HALF + TWO_BARS);
+            testPlayerTemperatureBar(singleplayer, context, maxTemperature, FULL + TWO_BARS);
         }
     }
 
-    private static void testPlayerTemperatureBar(
+    private void testPlayerTemperatureBar(
             TestSingleplayerContext singleplayer,
             ClientGameTestContext context,
             int temperature,
             String template
     ) {
-        singleplayer.getServer().runCommand("/gamerule fireDamage false"); // hack to prevent damage from affecting health underneath temperature display
         singleplayer.getServer().runCommand("/thermoo temperature set @p " + temperature);
         singleplayer.getServer().runCommand("/effect clear @p"); // hack to remove the poison temperature effect
 
         context.waitTicks(1); // wait to allow for temperature to update
         context.assertScreenshotEquals(
                 TestScreenshotComparisonOptions.of(template)
-                        .withRegion(842, 944, 162, 20)
+                        .withRegion(842, 944 - offset, 162, 20 + offset)
                         .save()
         );
+        singleplayer.getServer().runCommand("/thermoo temperature get @p");
     }
 }
