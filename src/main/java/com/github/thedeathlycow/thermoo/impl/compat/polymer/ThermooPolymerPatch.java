cb1d244335a10ad88b7e5673f058c7b67dadae22
TheDeathlyCow <thedeathlycow@gmail.com>
2025-08-09 03:24:36 -1000

    polymerize attributes

--- src/main/java/com/github/thedeathlycow/thermoo/impl/compat/polymer/ThermooPolymerPatch.java
+++ src/main/java/com/github/thedeathlycow/thermoo/impl/compat/polymer/ThermooPolymerPatch.java
@@ -1,13 +1,23 @@
 package com.github.thedeathlycow.thermoo.impl.compat.polymer;
 
 import com.github.thedeathlycow.thermoo.impl.Thermoo;
+import com.github.thedeathlycow.thermoo.impl.attribute.AttributeData;
 import com.github.thedeathlycow.thermoo.impl.compat.init.DependentServerModInitializer;
+import eu.pb4.polymer.core.api.entity.PolymerEntityUtils;
 
 public class ThermooPolymerPatch implements DependentServerModInitializer {
     @Override
     public void onInitializeServer() {
         if (Thermoo.getConfig().enablePolymerPatch()) {
-            
+            polymerizeAttributes();
+
+            Thermoo.LOGGER.info("Patched Thermoo for server-side with Polymer!");
+        }
+    }
+
+    private static void polymerizeAttributes() {
+        for (AttributeData data : AttributeData.values()) {
+            PolymerEntityUtils.registerAttribute(data.attribute());
         }
     }
 
