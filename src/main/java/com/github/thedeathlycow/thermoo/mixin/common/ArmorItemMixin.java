779503a66d7a172e42e52cbd66a23a6cdab7445a
TheDeathlyCow <thedeathlycow@gmail.com>
2025-11-04 18:55:33 -1000

    fix javadoc and unit tests

--- src/main/java/com/github/thedeathlycow/thermoo/mixin/common/ArmorItemMixin.java
+++ src/main/java/com/github/thedeathlycow/thermoo/mixin/common/ArmorItemMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.core.Holder;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,7 +19,8 @@ public abstract class ArmorItemMixin {
     @Inject(
             method = "method_56689",
             at = @At(
-                    value = "TAIL"
+                    value = "TAIL",
+                    shift = At.Shift.BEFORE // required to shift to before the builder is actually built
             )
     )
     private static void buildAttributeValues(
