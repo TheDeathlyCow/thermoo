2543dd7276acdd3b47cd91e0c0a2fc90890b4752
TheDeathlyCow <thedeathlycow@gmail.com>
2026-02-28 03:00:00 -1000

    fix merge errors

--- src/main/java/com/github/thedeathlycow/thermoo/impl/Thermoo.java
+++ src/main/java/com/github/thedeathlycow/thermoo/impl/Thermoo.java
@@ -12,17 +12,14 @@ import com.github.thedeathlycow.thermoo.impl.command.TemperatureCommand;
 import com.github.thedeathlycow.thermoo.impl.compat.init.DependentModInitializer;
 import com.github.thedeathlycow.thermoo.impl.config.ThermooConfig;
 import com.github.thedeathlycow.thermoo.impl.environment.EnvironmentLookupImpl;
-import com.github.thedeathlycow.thermoo.impl.temperature.effect.TemperatureEffectLoader;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.ArgumentTypeRegistry;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.registry.DynamicRegistries;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.commands.synchronization.ArgumentTypeInfo;
 import net.minecraft.commands.synchronization.SingletonArgumentInfo;
 import net.minecraft.resources.Identifier;
-import net.minecraft.server.packs.PackType;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
@@ -38,8 +35,8 @@ public class Thermoo implements ModInitializer {
 
     public static final ArgumentTypeInfo<
             HeatingModeArgumentType,
-                SingletonArgumentInfo<HeatingModeArgumentType>.Template
-                > HEATING_MODE_ARG_SERIALIZER = SingletonArgumentInfo.contextFree(HeatingModeArgumentType::heatingMode);
+            SingletonArgumentInfo<HeatingModeArgumentType>.Template
+            > HEATING_MODE_ARG_SERIALIZER = SingletonArgumentInfo.contextFree(HeatingModeArgumentType::heatingMode);
 
 
     public static final ArgumentTypeInfo<
