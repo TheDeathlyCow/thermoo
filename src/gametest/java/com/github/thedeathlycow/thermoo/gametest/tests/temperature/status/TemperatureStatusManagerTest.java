59603029237b91d257d2baaa113ca138d155446d
TheDeathlyCow <thedeathlycow@gmail.com>
2026-03-02 04:04:11 -1000

    implement game tests

--- src/gametest/java/com/github/thedeathlycow/thermoo/gametest/tests/temperature/status/TemperatureStatusManagerTest.java
+++ src/gametest/java/com/github/thedeathlycow/thermoo/gametest/tests/temperature/status/TemperatureStatusManagerTest.java
@@ -2,6 +2,7 @@ package com.github.thedeathlycow.thermoo.gametest.tests.temperature.status;
 
 import com.github.thedeathlycow.thermoo.api.ThermooRegistryKeys;
 import com.github.thedeathlycow.thermoo.api.temperature.status.v2.TemperatureStatus;
+import com.github.thedeathlycow.thermoo.gametest.ThermooTestMod;
 import com.github.thedeathlycow.thermoo.impl.temperature.status.TemperatureStatusManager;
 import net.fabricmc.fabric.api.gametest.v1.GameTest;
 import net.minecraft.core.Holder;
@@ -16,18 +17,51 @@ import java.util.List;
 @SuppressWarnings("unused")
 public class TemperatureStatusManagerTest {
     @GameTest
-    public void desert_dry_fallback_humidity_is_spring(GameTestHelper helper) {
+    public void applicationOrderSetsLookupOrder(GameTestHelper helper) {
         ServerLevel level = helper.getLevel();
         HolderLookup<TemperatureStatus> lookup = level.holderLookup(ThermooRegistryKeys.TEMPERATURE_STATUS);
 
-        var statuses = TemperatureStatusManager.lookup(EntityType.PLAYER.builtInRegistryHolder(), lookup);
+        List<Holder.Reference<TemperatureStatus>> statuses = TemperatureStatusManager.lookup(EntityType.PLAYER.builtInRegistryHolder(), lookup);
 
-//        List<Holder.Reference<TemperatureStatus>> expected = List.of(
-//                lookup.getOrThrow(ResourceKey.create())
-//        );
-//
-//        helper.assertValueEqual(statuses, );
+        List<Holder.Reference<TemperatureStatus>> expected = List.of(
+                lookup.getOrThrow(key("ordered/first")),
+                lookup.getOrThrow(key("ordered/second")),
+                lookup.getOrThrow(key("ordered/third")),
+                lookup.getOrThrow(key("ordered/fourth")),
+                lookup.getOrThrow(key("attribute_test")),
+                lookup.getOrThrow(key("function_test")),
+                lookup.getOrThrow(key("damage_test")),
+                lookup.getOrThrow(key("mod_exists_test")),
+                lookup.getOrThrow(key("mob_effect_test")),
+                lookup.getOrThrow(key("scaling_attribute_test")) // tests that items not in tag are last
+        );
+
+        helper.assertValueEqual(statuses, expected, "Temperature Status Order");
 
         helper.succeed();
+    }
+
+    @GameTest
+    public void modDoesNotExistTestIsNotLoaded(GameTestHelper helper) {
+        ServerLevel level = helper.getLevel();
+        HolderLookup<TemperatureStatus> lookup = level.holderLookup(ThermooRegistryKeys.TEMPERATURE_STATUS);
+
+        helper.assertTrue(lookup.get(key("mod_does_not_exist_test")).isEmpty(), "Effect did not respect load conditions");
+
+        helper.succeed();
+    }
+
+    @GameTest
+    public void modExistsTestIsLoaded(GameTestHelper helper) {
+        ServerLevel level = helper.getLevel();
+        HolderLookup<TemperatureStatus> lookup = level.holderLookup(ThermooRegistryKeys.TEMPERATURE_STATUS);
+
+        helper.assertTrue(lookup.get(key("mod_exists_test")).isPresent(), "Effect did not respect load conditions");
+
+        helper.succeed();
+    }
+
+    private ResourceKey<TemperatureStatus> key(String name) {
+        return ResourceKey.create(ThermooRegistryKeys.TEMPERATURE_STATUS, ThermooTestMod.id(name));
     }
 }
