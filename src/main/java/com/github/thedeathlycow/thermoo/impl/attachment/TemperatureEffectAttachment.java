a62c4590dee05bea51d401f6665a6253c25e6f9b
TheDeathlyCow <53499406+TheDeathlyCow@users.noreply.github.com>
2026-03-09 03:19:26 -1000

    Update Neoforge with 4.8.0 (#108)
    * fix publish file
    
    * Delete fabric.mod.json
    
    * [1.21.1] Temperature Effect Enable API (#105)
    
    * Temperature Effect Enable API (#94)
    
    * settings enabled
    
    * settings codec
    
    * add command
    
    * separate exception type for failed to disable
    
    * only change effect enable if it is different from existing value
    
    * throw if effect failed to enable for any entity
    
    * fix identifier/resource location issues
    
    * fix some temperature effects not removing themselves properly
    
    * remove effects immediately when disabled
    
    * fix attachment
    
    * bump neoforge
    
    * update kotlin

--- src/main/java/com/github/thedeathlycow/thermoo/impl/attachment/TemperatureEffectAttachment.java
+++ src/main/java/com/github/thedeathlycow/thermoo/impl/attachment/TemperatureEffectAttachment.java
@@ -2,35 +2,132 @@ package com.github.thedeathlycow.thermoo.impl.attachment;
 
 import com.github.thedeathlycow.thermoo.api.temperature.effects.ConfiguredTemperatureEffect;
 import com.github.thedeathlycow.thermoo.impl.temperature.effect.TemperatureEffectManager;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtOps;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.LivingEntity;
+import net.neoforged.neoforge.attachment.IAttachmentHolder;
+import net.neoforged.neoforge.common.util.INBTSerializable;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public final class TemperatureEffectAttachment {
+public final class TemperatureEffectAttachment implements INBTSerializable<CompoundTag> {
     private final Map<ResourceLocation, Settings> effectsSettings = new HashMap<>();
 
-    public void serverTick(LivingEntity provider) {
-        var availableEffects = TemperatureEffectManager.INSTANCE.getEffectsEntriesForEntity(provider);
+    private final IAttachmentHolder provider;
+
+    public TemperatureEffectAttachment(IAttachmentHolder provider) {
+        this.provider = provider;
+    }
+
+    public boolean setEffectEnabled(ResourceLocation effectId, boolean enabled) {
+        if (!(this.provider instanceof LivingEntity providerEntity)) {
+            return false;
+        }
+
+        Settings settings = this.getSettingsChecked(effectId);
+
+        if (settings != null && settings.enabled() != enabled) {
+            settings.setEnabled(enabled);
+
+            // this is meant to ensure that the effect is cleaned up right away and not have to wait for the next
+            // interval check, especially if that interval is long.
+            if (!settings.enabled() && settings.applied()) {
+                ConfiguredTemperatureEffect<?> effect = TemperatureEffectManager.INSTANCE.getEffect(effectId);
+                if (effect != null) {
+                    effect.remove(providerEntity);
+                    settings.setApplied(false);
+                }
+            }
+
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isEffectEnabled(ResourceLocation effectId) {
+        Settings settings = this.getSettingsChecked(effectId);
+
+        if (settings != null) {
+            return settings.enabled();
+        }
+
+        return false;
+    }
+
+
+    @Override
+    public CompoundTag serializeNBT(HolderLookup.Provider provider) {
+        var tag = new CompoundTag();
+
+        Settings.MAP_CODEC.encodeStart(NbtOps.INSTANCE, this.effectsSettings).ifSuccess(serializedMap -> {
+            tag.put(Settings.SETTINGS_KEY, serializedMap);
+        });
+
+        return tag;
+    }
+
+    @Override
+    public void deserializeNBT(HolderLookup.Provider provider, CompoundTag tag) {
+        this.effectsSettings.clear();
+
+        Settings.MAP_CODEC.decode(NbtOps.INSTANCE, tag).ifSuccess(result -> {
+            this.effectsSettings.putAll(result.getFirst());
+        });
+    }
+
+    public void serverTick() {
+        if (!(this.provider instanceof LivingEntity providerEntity)) {
+            return;
+        }
+
+        var availableEffects = TemperatureEffectManager.INSTANCE.getEffectsEntriesForEntity(providerEntity);
         for (TemperatureEffectManager.EntityTypeCacheEntry effectEntry : availableEffects) {
-            Settings settings = this.effectsSettings.computeIfAbsent(effectEntry.id(), ignored -> new Settings());
-            boolean wasApplied = settings.applied;
             ConfiguredTemperatureEffect<?> effect = effectEntry.effect();
-
-            if (effect.apply(provider)) {
-                settings.applied = true;
-            } else {
-                settings.applied = false;
-            }
-
-            if (wasApplied && !settings.applied) {
-                effect.remove(provider);
-            }
+            Settings settings = this.getSettings(effectEntry.id());
+            this.updateStatus(effect, settings);
         }
     }
 
-    private static class Settings {
-        private boolean applied = false;
+    private void updateStatus(ConfiguredTemperatureEffect<?> effect, Settings settings) {
+        if (settings.enabled()) {
+            if (!(this.provider instanceof LivingEntity providerEntity)) {
+                return;
+            }
+
+            boolean wasApplied = settings.applied();
+            boolean applied = effect.apply(providerEntity);
+
+            if (wasApplied && !applied) {
+                effect.remove(providerEntity);
+            }
+
+            settings.setApplied(applied);
+        }
+    }
+
+    @Nullable
+    private Settings getSettingsChecked(ResourceLocation effectId) {
+        if (!(this.provider instanceof LivingEntity providerEntity)) {
+            return null;
+        }
+
+        ConfiguredTemperatureEffect<?> effect = TemperatureEffectManager.INSTANCE.getEffect(effectId);
+
+        if (effect != null && (effect.entityTypes().size() == 0 || effect.entityTypes().contains(providerEntity.getType().builtInRegistryHolder()))) {
+            return this.getSettings(effectId);
+        } else {
+            return null;
+        }
+    }
+
+    private Settings getSettings(ResourceLocation effectId) {
+        return this.effectsSettings.computeIfAbsent(
+                effectId,
+                ignored -> new Settings(true)
+        );
     }
 }
