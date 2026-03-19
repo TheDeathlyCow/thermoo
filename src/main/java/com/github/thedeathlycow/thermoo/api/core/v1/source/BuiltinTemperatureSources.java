d17de1eb3264a0f169861c79780c2030d7ee68a9
TheDeathlyCow <thedeathlycow@gmail.com>
2026-03-18 19:52:45 -1000

    add convenience methods for creating temperature changes to ThermooLevel

--- src/main/java/com/github/thedeathlycow/thermoo/api/core/v1/source/BuiltinTemperatureSources.java
+++ src/main/java/com/github/thedeathlycow/thermoo/api/core/v1/source/BuiltinTemperatureSources.java
@@ -1,11 +1,16 @@
 package com.github.thedeathlycow.thermoo.api.core.v1.source;
 
 import com.github.thedeathlycow.thermoo.api.core.v1.TemperatureChange;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.TraceableEntity;
+import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.ApiStatus;
 
 /**
  * Stores shared temperature change contexts for the {@linkplain TemperatureSources built in temperature sources}. All
- * sources stored by this class have no cause, direct cause, or position.
+ * sources stored by this class have no cause, direct cause, or position. Also provides convenience methods for creating
+ * unshared instances from keys without needing to explicitly lookup sources.
  */
 @ApiStatus.NonExtendable
 public interface BuiltinTemperatureSources {
@@ -28,4 +33,40 @@ public interface BuiltinTemperatureSources {
      * @return Returns a shared temperature change context with a source of {@link TemperatureSources#ENVIRONMENT}
      */
     TemperatureChange environment();
+
+    /**
+     * Creates a simple temperature change context with only a type and no cause or position.
+     *
+     * @param sourceKey The key of the change source, may not be {@code null}.
+     */
+    TemperatureChange create(ResourceKey<TemperatureSource> sourceKey);
+
+    /**
+     * Creates a temperature change context with a type and a position, but no cause.
+     *
+     * @param sourceKey The key of the change source, may not be {@code null}.
+     * @param position  The position of the temperature change source, may not be {@code null}.
+     */
+    TemperatureChange create(ResourceKey<TemperatureSource> sourceKey, Vec3 position);
+
+    /**
+     * Creates a temperature change context with a type and a directCause entity. If the {@code directCause} is a
+     * {@link TraceableEntity} and has a non-null owner, then the {@link TemperatureChange#cause()} will refer to the owner. Otherwise,
+     * the {@link TemperatureChange#directCause()} and {@link TemperatureChange#cause()} will refer to the same entity.
+     * In either case, the {@link TemperatureChange#position()} will be the directCause's position.
+     *
+     * @param sourceKey   The key of the change source, may not be {@code null}.
+     * @param directCause The entity directly responsible for the change, may not be {@code null}.
+     */
+    TemperatureChange create(ResourceKey<TemperatureSource> sourceKey, Entity directCause);
+
+    /**
+     * Creates a temperature change context with a type, a cause entity, and a direct cause entity. The
+     * {@link TemperatureChange#position()} will be the direct cause's position.
+     *
+     * @param sourceKey   The key of the change source, may not be {@code null}.
+     * @param cause       The entity responsible for the change, may not be {@code null}.
+     * @param directCause The entity directly responsible for the change, may not be {@code null}.
+     */
+    TemperatureChange create(ResourceKey<TemperatureSource> sourceKey, Entity cause, Entity directCause);
 }
--- src/main/java/com/github/thedeathlycow/thermoo/impl/core/BuiltinTemperatureSourcesImpl.java
+++ src/main/java/com/github/thedeathlycow/thermoo/impl/core/BuiltinTemperatureSourcesImpl.java
@@ -4,25 +4,54 @@ import com.github.thedeathlycow.thermoo.api.core.v1.TemperatureChange;
 import com.github.thedeathlycow.thermoo.api.core.v1.source.BuiltinTemperatureSources;
 import com.github.thedeathlycow.thermoo.api.core.v1.source.TemperatureSource;
 import com.github.thedeathlycow.thermoo.api.core.v1.source.TemperatureSources;
+import net.minecraft.core.Holder;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.phys.Vec3;
 
 public record BuiltinTemperatureSourcesImpl(
         TemperatureChange absolute,
         TemperatureChange active,
         TemperatureChange passive,
-        TemperatureChange environment
+        TemperatureChange environment,
+        RegistryAccess access
 ) implements BuiltinTemperatureSources {
     public BuiltinTemperatureSourcesImpl(RegistryAccess access) {
         this(
-                change(access, TemperatureSources.ABSOLUTE),
-                change(access, TemperatureSources.ACTIVE),
-                change(access, TemperatureSources.PASSIVE),
-                change(access, TemperatureSources.ENVIRONMENT)
+                create(access, TemperatureSources.ABSOLUTE),
+                create(access, TemperatureSources.ACTIVE),
+                create(access, TemperatureSources.PASSIVE),
+                create(access, TemperatureSources.ENVIRONMENT),
+                access
         );
     }
 
-    private static TemperatureChange change(RegistryAccess access, ResourceKey<TemperatureSource> key) {
+    @Override
+    public TemperatureChange create(ResourceKey<TemperatureSource> sourceKey) {
+        return TemperatureChange.create(this.lookup(sourceKey));
+    }
+
+    @Override
+    public TemperatureChange create(ResourceKey<TemperatureSource> sourceKey, Vec3 position) {
+        return TemperatureChange.create(this.lookup(sourceKey), position);
+    }
+
+    @Override
+    public TemperatureChange create(ResourceKey<TemperatureSource> sourceKey, Entity directCause) {
+        return TemperatureChange.create(this.lookup(sourceKey), directCause);
+    }
+
+    @Override
+    public TemperatureChange create(ResourceKey<TemperatureSource> sourceKey, Entity cause, Entity directCause) {
+        return TemperatureChange.create(this.lookup(sourceKey), cause, directCause);
+    }
+
+    private Holder<TemperatureSource> lookup(ResourceKey<TemperatureSource> key) {
+        return access.getOrThrow(key);
+    }
+
+    private static TemperatureChange create(RegistryAccess access, ResourceKey<TemperatureSource> key) {
         return TemperatureChange.create(access.getOrThrow(key));
     }
 }
