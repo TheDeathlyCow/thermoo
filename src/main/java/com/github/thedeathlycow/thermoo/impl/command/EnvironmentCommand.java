de5a9ad0543e413b17e064101ec9c002634b244b
TheDeathlyCow <thedeathlycow@gmail.com>
2026-03-20 02:01:21 -1000

    bump version and add changelog

--- src/main/java/com/github/thedeathlycow/thermoo/impl/command/EnvironmentCommand.java
+++ src/main/java/com/github/thedeathlycow/thermoo/impl/command/EnvironmentCommand.java
@@ -152,7 +152,6 @@ public final class EnvironmentCommand {
                         .hasPermission(Permissions.COMMANDS_GAMEMASTER)))
                         .then(temperature)
                         .then(relativeHumidity)
-                        .then(relativeHumidityOld)
                         .then(pressure)
         );
     }
