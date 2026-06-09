9383602420b6e90e7a804198ce128da04c183cad
TheDeathlyCow <thedeathlycow@gmail.com>
2026-06-08 22:31:48 -1000

    add is cold enough to snow event to seasons api

--- src/main/java/com/github/thedeathlycow/thermoo/api/season/v2/ThermooSeason.java
+++ src/main/java/com/github/thedeathlycow/thermoo/api/season/v2/ThermooSeason.java
@@ -1,18 +1,25 @@
 package com.github.thedeathlycow.thermoo.api.season.v2;
 
+import dev.yumi.commons.TriState;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
 import net.minecraft.util.StringRepresentable;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biome;
 import org.jetbrains.annotations.ApiStatus;
 
-/**
- * Contains common functionality for all thermoo season types.
- *
- * @see TemperateSeason
- * @see TropicalSeason
- */
+/// Contains common functionality for all thermoo season types.
+///
+/// @see TemperateSeason
+/// @see TropicalSeason
 @ApiStatus.NonExtendable
 public sealed interface ThermooSeason extends StringRepresentable permits TemperateSeason, TropicalSeason {
-    /**
-     * Creates a season state that represents the start of this season.
-     */
+    /// Creates a season state that represents the start of this season.
     ThermooSeasonState<? extends ThermooSeason> createState();
+
+    /// Convenience method for invoking [ThermooSeasonEvents#IS_COLD_ENOUGH_TO_SNOW]
+    static TriState isColdEnoughToSnow(Level level, BlockPos pos) {
+        Holder<Biome> biome = level.getBiomeManager().getNoiseBiomeAtPosition(pos);
+        return ThermooSeasonEvents.IS_COLD_ENOUGH_TO_SNOW.invoker().isColdEnoughToSnow(level, pos, biome);
+    }
 }
--- src/main/java/com/github/thedeathlycow/thermoo/api/season/v2/ThermooSeasonEvents.java
+++ src/main/java/com/github/thedeathlycow/thermoo/api/season/v2/ThermooSeasonEvents.java
@@ -2,38 +2,37 @@ package com.github.thedeathlycow.thermoo.api.season.v2;
 
 import com.github.thedeathlycow.thermoo.api.environment.v2.attribute.ThermooEnvironmentAttributes;
 import com.github.thedeathlycow.thermoo.impl.Thermoo;
+import dev.yumi.commons.TriState;
 import dev.yumi.commons.event.Event;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.attribute.EnvironmentAttributeSystem;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biome;
 
 import java.util.Optional;
 
-/**
- * Events related to Seasons in Thermoo. Note that Thermoo will not provide any seasons mod functionality by itself,
- * that must be provided by an external seasons mod. This is primarily intended to be used for mod-agnostic seasons mod
- * integration.
- */
+/// Events related to Seasons in Thermoo. Note that Thermoo will not provide any seasons mod functionality by itself,
+/// that must be provided by an external seasons mod. This is primarily intended to be used for mod-agnostic seasons mod
+/// integration.
 public final class ThermooSeasonEvents {
     private ThermooSeasonEvents() {
 
     }
 
-    /**
-     * Retrieves the current temperate season state at a position in a level, if a season mod is loaded. Thermoo does
-     * not add seasons by itself, seasons must be implemented by another mod like Fabric Seasons or Serene Seasons. This
-     * event just places season integration into a common source.
-     * <p>
-     * If any listener returns a non-empty season state, then all further processing is cancelled and that state is
-     * returned.
-     * <p>
-     * If the queried position does not have seasons, or a seasons mod is not installed, then returns a state based
-     * on the current value of the {@linkplain ThermooEnvironmentAttributes environment attributes}.
-     *
-     * @see TemperateSeason#getCurrentState(Level, BlockPos)
-     * @see #GET_CURRENT_TROPICAL_SEASON
-     */
+    /// Retrieves the current temperate season state at a position in a level, if a season mod is loaded. Thermoo does
+    /// not add seasons by itself, seasons must be implemented by another mod like Fabric Seasons or Serene Seasons. This
+    /// event just places season integration into a common source.
+    ///
+    /// If any listener returns a non-empty season state, then all further processing is cancelled and that state is
+    /// returned.
+    ///
+    /// If the queried position does not have seasons, or a seasons mod is not installed, then returns a state based
+    /// on the current value of the {@linkplain ThermooEnvironmentAttributes environment attributes}.
+    ///
+    /// @see TemperateSeason#getCurrentState(Level, BlockPos)
+    /// @see #GET_CURRENT_TROPICAL_SEASON
     public static final Event<Identifier, CurrentSeasonCallback<TemperateSeason>> GET_CURRENT_SEASON = Thermoo.EVENT_MANAGER.create(
             CurrentSeasonCallback.class,
             callbacks -> (level, pos) -> {
@@ -54,20 +53,18 @@ public final class ThermooSeasonEvents {
             }
     );
 
-    /**
-     * Retrieves the current tropical season state at a position in a level, if a season mod is loaded. Thermoo does not
-     * add seasons by itself, seasons must be implemented by another mod like Fabric Seasons or Serene Seasons. This
-     * event just places season integration into a common source.
-     * <p>
-     * If any listener returns a non-empty season state, then all further processing is cancelled and that state is
-     * returned.
-     * <p>
-     * If the queried position does not have seasons, or a seasons mod is not installed, then returns a state based
-     * on the current value of the {@linkplain ThermooEnvironmentAttributes environment attributes}.
-     *
-     * @see TropicalSeason#getCurrentState(Level, BlockPos)
-     * @see #GET_CURRENT_SEASON
-     */
+    /// Retrieves the current tropical season state at a position in a level, if a season mod is loaded. Thermoo does not
+    /// add seasons by itself, seasons must be implemented by another mod like Fabric Seasons or Serene Seasons. This
+    /// event just places season integration into a common source.
+    ///
+    /// If any listener returns a non-empty season state, then all further processing is cancelled and that state is
+    /// returned.
+    ///
+    /// If the queried position does not have seasons, or a seasons mod is not installed, then returns a state based
+    /// on the current value of the {@linkplain ThermooEnvironmentAttributes environment attributes}.
+    ///
+    /// @see TropicalSeason#getCurrentState(Level, BlockPos)
+    /// @see #GET_CURRENT_SEASON
     public static final Event<Identifier, CurrentSeasonCallback<TropicalSeason>> GET_CURRENT_TROPICAL_SEASON = Thermoo.EVENT_MANAGER.create(
             CurrentSeasonCallback.class,
             callbacks -> (level, pos) -> {
@@ -88,8 +85,39 @@ public final class ThermooSeasonEvents {
             }
     );
 
+    /// A season-aware hook for checking if an area can be snowy.
+    ///
+    /// Snowy checks can be fickle and inconsistent between seasons mods, this provides a single method that will
+    /// determine if a seasons mod thinks it is cold enough to snow.
+    ///
+    /// This is not a substitute for vanilla checks! A default return indicates that callers should rely upon vanilla
+    /// checks instead. It is not defined exactly when this event will return default
+    ///
+    /// - Returns [true][TriState#TRUE] if any listener thinks the area is definitely snowy.
+    /// - Returns [false][TriState#FALSE] if any listener think the area definitely is not snowy.
+    /// - Returns [default][TriState#DEFAULT] if no listener believes the area must be snowy or not snowy, and callers
+    /// should fall back to vanilla processing.
+    public static final Event<Identifier, ColdEnoughToSnow> IS_COLD_ENOUGH_TO_SNOW = Thermoo.EVENT_MANAGER.create(
+            ColdEnoughToSnow.class,
+            listeners -> (level, pos, biome) -> {
+                for (ColdEnoughToSnow listener : listeners) {
+                    TriState isSnowy = listener.isColdEnoughToSnow(level, pos, biome);
+                    if (isSnowy != TriState.DEFAULT) {
+                        return isSnowy;
+                    }
+                }
+
+                return TriState.DEFAULT;
+            }
+    );
+
     @FunctionalInterface
     public interface CurrentSeasonCallback<S extends ThermooSeason> {
         Optional<ThermooSeasonState<S>> getCurrentSeasonState(Level level, BlockPos pos);
+    }
+
+    @FunctionalInterface
+    public interface ColdEnoughToSnow {
+        TriState isColdEnoughToSnow(Level level, BlockPos pos, Holder<Biome> biome);
     }
 }
