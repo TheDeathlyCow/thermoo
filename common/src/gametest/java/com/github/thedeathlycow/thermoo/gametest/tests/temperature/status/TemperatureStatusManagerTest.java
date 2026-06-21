678130d3cf355c2ce7d5cafb2245391e92b5e2fd
TheDeathlyCow <thedeathlycow@gmail.com>
2026-06-20 19:34:48 -1000

    add sources back to test runs

--- common/src/gametest/java/com/github/thedeathlycow/thermoo/gametest/tests/temperature/status/TemperatureStatusManagerTest.java
+++ common/src/gametest/java/com/github/thedeathlycow/thermoo/gametest/tests/temperature/status/TemperatureStatusManagerTest.java
@@ -8,6 +8,7 @@ import com.github.thedeathlycow.thermoo.gametest.util.GameTest;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.gametest.framework.GameTestHelper;
+import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.EntityType;
@@ -21,21 +22,26 @@ public class TemperatureStatusManagerTest {
         ServerLevel level = helper.getLevel();
         HolderLookup<TemperatureStatus> lookup = level.holderLookup(ThermooRegistries.TEMPERATURE_STATUS);
 
-        List<Holder.Reference<TemperatureStatus>> statuses = TemperatureStatusManager.lookup(EntityType.PLAYER.builtInRegistryHolder(), lookup);
+        List<Identifier> statuses = TemperatureStatusManager.lookup(EntityType.PLAYER.builtInRegistryHolder(), lookup)
+                .stream()
+                .map(ref -> ref.key().identifier())
+                .toList();
 
-        List<Holder.Reference<TemperatureStatus>> expected = List.of(
-                lookup.getOrThrow(key("ordered/first")),
-                lookup.getOrThrow(key("ordered/second")),
-                lookup.getOrThrow(key("ordered/third")),
-                lookup.getOrThrow(key("ordered/fourth")),
-                lookup.getOrThrow(key("attribute_test")),
-                lookup.getOrThrow(key("function_test")),
-                lookup.getOrThrow(key("damage_test")),
-                lookup.getOrThrow(key("mod_exists_test")),
-                lookup.getOrThrow(key("mob_effect_test")),
-                lookup.getOrThrow(key("disabled_by_default")),
-                lookup.getOrThrow(key("scaling_attribute_test")) // tests that items not in tag are last
-        );
+        List<Identifier> expected = List.of(
+                        lookup.getOrThrow(key("ordered/first")),
+                        lookup.getOrThrow(key("ordered/second")),
+                        lookup.getOrThrow(key("ordered/third")),
+                        lookup.getOrThrow(key("ordered/fourth")),
+                        lookup.getOrThrow(key("attribute_test")),
+                        lookup.getOrThrow(key("function_test")),
+                        lookup.getOrThrow(key("damage_test")),
+                        lookup.getOrThrow(key("mod_exists_test")),
+                        lookup.getOrThrow(key("mob_effect_test")),
+                        lookup.getOrThrow(key("disabled_by_default")),
+                        lookup.getOrThrow(key("scaling_attribute_test")) // tests that items not in tag are last
+                ).stream()
+                .map(ref -> ref.key().identifier())
+                .toList();
 
         helper.assertValueEqual(statuses, expected, "Temperature Status Order");
 
